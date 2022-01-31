e828f28237a51888b77f8a0406278323379a4e18
hypothe <markeagle98@gmail.com>
2022-01-31 23:37:39 +0100

    <Edit> typo

--- set_server_name.sh
+++ set_server_name.sh
@@ -2,4 +2,4 @@ SERVER_NAME="${BOMI_SERVER_NAME:-localhost}"
 echo $SERVER_NAME
 file="index.html"
 
-awk -v awk_var=$SERVER_NAME '{gsub(/localhost/, awk_var); print $0}' $file > .tmp && mv .tmp $file; \
+awk -v awk_var=$SERVER_NAME '{gsub(/localhost/, awk_var); print $0}' $file > .tmp && mv .tmp $file;
